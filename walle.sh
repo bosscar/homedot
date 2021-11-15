b1f8f325f7b60919c93b2046716f0b1de8488ad0
bosscar <bosscar@gmail.com>
2021-11-15 02:49:15 -0800

    walle update

--- walle.sh
+++ walle.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export DISPLAY=:0
 WAL=$(ls /media/edrive/wallpapers | shuf -n 1)
 echo "/media/edrive/wallpapers/$WAL" >> ~/.config/wallpapers.log
 /home/bosscar/.local/bin/wal -i "/media/edrive/wallpapers/$WAL" &
